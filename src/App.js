5e36ad2603bc1b00183c0e88e62afa1b536b1716
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2021-03-21 22:09:38 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import './App.css'; 
+import Rotas from './components/Rotas'; 
+
+const App =() =>(
+  <div className="App">
+    <Rotas />
+  </div>
+); 
+
+
+export default App; 
